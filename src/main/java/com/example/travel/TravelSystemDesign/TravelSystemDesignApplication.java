69ba22b55e3b8bdab95d4580aba805b37cfbf4bc
shadan30 <37139445+shadan30@users.noreply.github.com>
2024-03-08 00:32:06 +0530

    First Commit

---
+++ src/main/java/com/example/travel/TravelSystemDesign/TravelSystemDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel.TravelSystemDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSystemDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSystemDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travel/TravelSystemDesign/TravelSystemDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel.TravelSystemDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSystemDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
